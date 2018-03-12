e46ca8765aa006e1385884b974544ffcc9f3adde
AntonescuAlexandru <antonescu.alexandru23@gmail.com>
2018-03-12 05:07:41 +0200

    Puzzle

---
+++ _puzzle/Form1.Designer.cs
@@ -0,0 +1,297 @@
+﻿namespace _puzzle
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_group = new System.Windows.Forms.Panel();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button15 = new System.Windows.Forms.Button();
+            this.button14 = new System.Windows.Forms.Button();
+            this.button13 = new System.Windows.Forms.Button();
+            this.button12 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.browse_btn = new System.Windows.Forms.Button();
+            this.restart = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btn_group.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btn_group
+            // 
+            this.btn_group.Controls.Add(this.button8);
+            this.btn_group.Controls.Add(this.button4);
+            this.btn_group.Controls.Add(this.button15);
+            this.btn_group.Controls.Add(this.button14);
+            this.btn_group.Controls.Add(this.button13);
+            this.btn_group.Controls.Add(this.button12);
+            this.btn_group.Controls.Add(this.button11);
+            this.btn_group.Controls.Add(this.button10);
+            this.btn_group.Controls.Add(this.button9);
+            this.btn_group.Controls.Add(this.button7);
+            this.btn_group.Controls.Add(this.button6);
+            this.btn_group.Controls.Add(this.button5);
+            this.btn_group.Controls.Add(this.button3);
+            this.btn_group.Controls.Add(this.button2);
+            this.btn_group.Controls.Add(this.button1);
+            this.btn_group.Location = new System.Drawing.Point(12, 12);
+            this.btn_group.Name = "btn_group";
+            this.btn_group.Size = new System.Drawing.Size(280, 280);
+            this.btn_group.TabIndex = 0;
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(210, 70);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(70, 70);
+            this.button8.TabIndex = 6;
+            this.button8.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(210, 0);
+            this.button4.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(70, 70);
+            this.button4.TabIndex = 3;
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button15
+            // 
+            this.button15.Location = new System.Drawing.Point(140, 210);
+            this.button15.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button15.Name = "button15";
+            this.button15.Size = new System.Drawing.Size(70, 70);
+            this.button15.TabIndex = 13;
+            this.button15.UseVisualStyleBackColor = true;
+            // 
+            // button14
+            // 
+            this.button14.Location = new System.Drawing.Point(70, 210);
+            this.button14.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button14.Name = "button14";
+            this.button14.Size = new System.Drawing.Size(70, 70);
+            this.button14.TabIndex = 12;
+            this.button14.UseVisualStyleBackColor = true;
+            // 
+            // button13
+            // 
+            this.button13.Location = new System.Drawing.Point(0, 210);
+            this.button13.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button13.Name = "button13";
+            this.button13.Size = new System.Drawing.Size(70, 70);
+            this.button13.TabIndex = 11;
+            this.button13.UseVisualStyleBackColor = true;
+            // 
+            // button12
+            // 
+            this.button12.Location = new System.Drawing.Point(210, 140);
+            this.button12.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button12.Name = "button12";
+            this.button12.Size = new System.Drawing.Size(70, 70);
+            this.button12.TabIndex = 10;
+            this.button12.UseVisualStyleBackColor = true;
+            // 
+            // button11
+            // 
+            this.button11.Location = new System.Drawing.Point(140, 140);
+            this.button11.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(70, 70);
+            this.button11.TabIndex = 9;
+            this.button11.UseVisualStyleBackColor = true;
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(70, 140);
+            this.button10.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(70, 70);
+            this.button10.TabIndex = 8;
+            this.button10.UseVisualStyleBackColor = true;
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(0, 140);
+            this.button9.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(70, 70);
+            this.button9.TabIndex = 7;
+            this.button9.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(140, 70);
+            this.button7.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(70, 70);
+            this.button7.TabIndex = 5;
+            this.button7.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(70, 70);
+            this.button6.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(70, 70);
+            this.button6.TabIndex = 4;
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(0, 70);
+            this.button5.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(70, 70);
+            this.button5.TabIndex = 3;
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(140, 0);
+            this.button3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(70, 70);
+            this.button3.TabIndex = 2;
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(70, 0);
+            this.button2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(70, 70);
+            this.button2.TabIndex = 1;
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(210, 210);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(70, 70);
+            this.button1.TabIndex = 0;
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(392, 10);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(280, 280);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // browse_btn
+            // 
+            this.browse_btn.Location = new System.Drawing.Point(289, 306);
+            this.browse_btn.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.browse_btn.Name = "browse_btn";
+            this.browse_btn.Size = new System.Drawing.Size(83, 26);
+            this.browse_btn.TabIndex = 7;
+            this.browse_btn.Text = "Browse";
+            this.browse_btn.UseVisualStyleBackColor = true;
+            this.browse_btn.Click += new System.EventHandler(this.browse_btn_Click);
+            // 
+            // restart
+            // 
+            this.restart.Location = new System.Drawing.Point(305, 283);
+            this.restart.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.restart.Name = "restart";
+            this.restart.Size = new System.Drawing.Size(56, 19);
+            this.restart.TabIndex = 10;
+            this.restart.Text = "Restart";
+            this.restart.UseVisualStyleBackColor = true;
+            this.restart.Click += new System.EventHandler(this.restart_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(40, 318);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(692, 352);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.restart);
+            this.Controls.Add(this.browse_btn);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btn_group);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.btn_group.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel btn_group;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button15;
+        private System.Windows.Forms.Button button14;
+        private System.Windows.Forms.Button button13;
+        private System.Windows.Forms.Button button12;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button browse_btn;
+        private System.Windows.Forms.Button restart;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ _puzzle/Form1.cs
@@ -0,0 +1,220 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _puzzle
+{
+    public partial class Form1 : Form
+    {
+        int clicks = 0;
+        Point EmptyPoint;
+        Image ToBeResize = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            pictureBox1.BackgroundImage = ToBeResize;
+            EmptyPoint.X = 0;
+            EmptyPoint.Y = 0;
+            button1.Click += MoveButton;
+            button2.Click += MoveButton;
+            button3.Click += MoveButton;
+            button4.Click += MoveButton;
+            button5.Click += MoveButton;
+            button6.Click += MoveButton;
+            button7.Click += MoveButton;
+            button8.Click += MoveButton;
+            button9.Click += MoveButton;
+            button10.Click += MoveButton;
+            button11.Click += MoveButton;
+            button12.Click += MoveButton;
+            button13.Click += MoveButton;
+            button14.Click += MoveButton;
+            button15.Click += MoveButton;
+            label1.Text = "Idle";
+            button1.Enabled = false;
+            button2.Enabled = false;
+            button3.Enabled = false;
+            button4.Enabled = false;
+            button5.Enabled = false;
+            button6.Enabled = false;
+            button7.Enabled = false;
+            button8.Enabled = false;
+            button9.Enabled = false;
+            button10.Enabled = false;
+            button11.Enabled = false;
+            button12.Enabled = false;
+            button13.Enabled = false;
+            button14.Enabled = false;
+            button15.Enabled = false;
+            restart.Enabled = false;
+            
+      
+        }
+
+        private void MoveButton(object sender, EventArgs e)
+        {
+            int x = EmptyPoint.X - ((Button)sender).Location.X;
+            int px = x < 0 ? -x : x;
+
+            int y = EmptyPoint.Y - ((Button)sender).Location.Y;
+            int py = y < 0 ? -y : y;
+
+            if (((Button)sender).Location.Y.Equals(EmptyPoint.Y) && px.Equals(((Button)sender).Size.Width)) //Butonul se va deplasa pe orizontala
+            {
+
+                ((Button)sender).Location = new Point(((Button)sender).Location.X + x, ((Button)sender).Location.Y);
+                EmptyPoint.X -= x;
+                clicks++;
+            }
+            if (((Button)sender).Location.X.Equals(EmptyPoint.X) && py.Equals(((Button)sender).Size.Height))//Butonul se va deplasa pe verticala
+            {
+                ((Button)sender).Location = new Point(((Button)sender).Location.X, ((Button)sender).Location.Y + y);
+                EmptyPoint.Y -= y;
+                clicks++;  
+            }
+            label1.Text = "Clicks : " + clicks;
+        
+        }
+
+        private void browse_btn_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+
+            openFileDialog1.Filter = "Image Files (JPEG,GIF,BMP,PNG,JPG)|*.jpeg;*.bmp;*.png;*.jpg";
+
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                ToBeResize = Image.FromFile(openFileDialog1.FileName);
+                pictureBox1.Image = Resize(ToBeResize);
+                AddImagesToButtons(ReturnCroppedList(Resize(ToBeResize), 70, 70));
+                button1.Enabled = true;
+                button2.Enabled = true;
+                button3.Enabled = true;
+                button4.Enabled = true;
+                button5.Enabled = true;
+                button6.Enabled = true;
+                button7.Enabled = true;
+                button8.Enabled = true;
+                button9.Enabled = true;
+                button10.Enabled = true;
+                button11.Enabled = true;
+                button12.Enabled = true;
+                button13.Enabled = true;
+                button14.Enabled = true;
+                button15.Enabled = true;
+                restart.Enabled = true;
+                restart.Enabled = true;
+            }
+        }
+
+
+        private Bitmap Resize (Image img) {
+            int w = 280;
+            int h = 280;
+
+            Bitmap bitmap = new Bitmap(w, h);
+            Graphics graphic = Graphics.FromImage((Image)bitmap);
+            //graphic.InterpolationMode = InterpolationMode.HighQualityBicubic;
+            graphic.DrawImage(img, 0, 0, w, h);
+            return bitmap;    
+        }
+
+        public ArrayList ReturnCroppedList(Bitmap ToBeCropped, int x, int y) {
+
+            ArrayList pictures = new ArrayList();
+            int dreapta = 0;
+            int jos = 0;
+
+
+            for (int k = 0; k < 15; k++)
+            {
+                Bitmap piece = new Bitmap(x, y);
+
+                for (int i = 0; i < x; i++)
+                    for (int j = 0; j < y; j++)
+                        piece.SetPixel(i, j, ToBeCropped.GetPixel(i + dreapta, j + jos));
+
+                pictures.Add(piece);
+                dreapta += 70;
+
+                if (dreapta == 280) { dreapta = 0; jos += 70; }
+                if (jos == 280) { break; }
+
+            }
+            return pictures;
+        }
+        private void AddImagesToButtons(ArrayList pictures) {
+            int count = 0;
+            int[] c = new int[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14 };
+            Amesteca(c);
+            foreach (Button b in btn_group.Controls)
+            {
+                if (count < c.Length)
+                {
+                    b.Image = (Image)pictures[c[count]];
+                    count++;
+                }
+            }
+        }
+        private void Amesteca(int[] array)
+        {
+            Random r = new Random();
+
+            int start = r.Next(1, array.Length);
+            for (int i = 0; i < array.Length; i++)
+                for (int j = start; j > 0; j--)
+                {
+                    int temp = array[i];
+                    array[i] = array[j];
+                    array[j] = temp;
+                }
+         //   return array;
+            
+        }
+
+        private void restart_Click(object sender, EventArgs e)
+        {
+            AddImagesToButtons(ReturnCroppedList(Resize(ToBeResize), 70, 70));
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+   
+
+        /*private void MoveButton(Button button)
+        {
+            int x = EmptyPoint.X - button.Location.X;
+            int px = x < 0 ? -x : x;
+
+            int y = EmptyPoint.Y - button.Location.Y;
+            int py = y < 0 ? -y : y;
+
+            if (button.Location.Y.Equals(EmptyPoint.Y) && px.Equals(button.Size.Width))
+            {
+
+                button.Location = new Point(button.Location.X + x, button.Location.Y);
+                EmptyPoint.X -= x;
+
+
+            }
+            if (button.Location.X.Equals(EmptyPoint.X) && py.Equals(button.Size.Width))
+            {
+                button.Location = new Point(button.Location.X, button.Location.Y + y);
+                EmptyPoint.Y -= y;
+            }
+        }*/
+    }
+}
